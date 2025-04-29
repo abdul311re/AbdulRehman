c9be1475112df081c7fd5090b2e2c2d77f8729ea
ABDUL REHMAN <abdul.reh311@gmail.com>
2025-04-30 04:59:49 +0500

    Updates

--- assets/Mobile-DU36x5va.js
+++ assets/Mobile-DU36x5va.js
@@ -1,4 +1,4 @@
-import{j as e,F as l,c as i,r}from"./index-S5isQ9Xq.js";import{d as o}from"./styled-components.browser.esm-FWkoXi19.js";import{G as p,B as c}from"./button-CvpmLDyz.js";import{S as d}from"./scrollAnimation.min-DcUvECGc.js";/* empty css                */import{B as m}from"./Breadcrumb-BSBZ0zDg.js";const h="/AbdulRehman/assets/MobileHero-CW_2VXm-.svg",x=[{label:"Home",href:"/"},{label:"Services"},{label:"Mobile App Development",href:"/Mobile"}],u=o.div`
+import{j as e,F as l,c as i,r}from"./index-5Y1lqBge.js";import{d as o}from"./styled-components.browser.esm-GN1SOHzW.js";import{G as p,B as c}from"./button-K9qncs5Y.js";import{S as d}from"./scrollAnimation.min-BLa7DwES.js";/* empty css                */import{B as m}from"./Breadcrumb-DkmViDIL.js";const h="/AbdulRehman/assets/MobileHero-CW_2VXm-.svg",x=[{label:"Home",href:"/"},{label:"Services"},{label:"Mobile App Development",href:"/Mobile"}],u=o.div`
     display:flex;
     align-items:center;
     justify-content:center;
